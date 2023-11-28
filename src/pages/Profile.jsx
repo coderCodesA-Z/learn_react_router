f194f2e25a3f811806fe8c70abbf80960594f956
Sayan Dutta <rockistuart@gmail.com>
2023-11-28 20:00:02 +0530

    navigating using Link component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,35 @@
-
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+	return (
+		<div>
+			<div className="flex justify-between items-center px-10 py-4">
+				<h4 className="text-2xl font-bold">Logo</h4>
+				<ul className="flex space-x-2 p-4 ">
+					<li>
+						<Link
+							to={"/"}
+							className="px-4 py-3 rounded-md text-white"
+						>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link
+							to={"/profile"}
+							className="px-4 py-3 rounded-md text-white"
+						>
+							Profile
+						</Link>
+					</li>
+				</ul>
+			</div>
 
-export default Profile
+			<div className="h-[calc(100vh-88px)] flex justify-center items-center">
+				<h1 className="font-mono text-8xl">Profile</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
