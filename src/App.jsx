8cc060c11f76a38f82919191c472a61edd5831ad
Sayan Dutta <rockistuart@gmail.com>
2023-11-28 19:30:44 +0530

    create routes using createRouteFromElements and createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 // myapp.com
-const appRouter = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-	},
-	{ path: "/profile", element: <Profile /> },
-]);
+// const appRouter = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <Home />,
+// 	},
+// 	{ path: "/profile", element: <Profile /> },
+// ]);
+
+const appRoutesDefinition = createRoutesFromElements(
+	<Route>
+		<Route path="/" element={<Home />} />
+		<Route path="/profile" element={<Profile />} />
+	</Route>
+);
+
+const appRouter = createBrowserRouter(appRoutesDefinition);
+
 const App = () => {
 	return <RouterProvider router={appRouter}>App</RouterProvider>;
 };
